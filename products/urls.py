7789229fbdab61e0792d62973d7cfc96bc5e4baf
JOO-10000-Zi <j4615545@gmail.com>
2023-02-02 15:15:37 +0900

    feat : Products 모델, 폼 생성

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ app_name = "products"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('product_create', views.create, name="product_create"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import ProductsForm, ProductsInfoForm
+from .models import Products, ProductsInfo
+
 
 # Create your views here.
 def index(request):
     return render(request, "products/index.html")
-    
+
+def create(request):
+    if request.method == "POST":
+        create_form = ProductsForm(request.POST, request.FILES)
+        product_images = request.FILES.getlist("image")
+        if create_form.is_valid():
+            product = create_form.save()
+            for img in product_images:
+                ProductsInfoForm.objects.create(product=product, image=img)
+            return redirect("/")
+    else:
+        create_form = ProductsForm()
+        product_image_form = ProductsInfoForm()
+
+    context = {
+        "create_form": create_form,
+        "product_image_form": product_image_form,
+    }
+
+    return render(request, "products/products_create.html", context)
