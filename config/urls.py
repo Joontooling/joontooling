0036a81af89eb62d071869a637f208051cca4298
nahkim <nahkim@student.42seoul.kr>
2023-02-02 16:10:20 +0900

    feat : community app 및 모델 생성

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('products/', include("products.urls")),
+    path('community/', include("community.urls")),
 ]
