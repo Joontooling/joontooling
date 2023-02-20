1e6651d1fde5a71549670fad030422926070bbf6
byeongjin <qudwls7468@gmail.com>
2023-02-20 19:12:52 +0900

    Fix combine two navigation bars into one

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,40 @@
 const header = document.querySelector('.header');
-const navFix = document.querySelector('#nav--fixed');
 const headerHeight = header.offsetHeight;
-const navHeight = 63;
+const navHeight = 70;
 
-const categoryMenuIcons = document.querySelectorAll('.inner__menu__icon');
+const navi = document.querySelector('.nav');
+const naviWrapper = navi.querySelector('.nav-wrapper');
+const innerMenu = navi.querySelector('.inner__menu');
+
+const categoryMenuIcon = document.querySelector('.inner__menu__icon');
 const category = document.querySelector('.category__wrap');
 
 window.addEventListener('scroll', () => {
   let scrollLocation = document.documentElement.scrollTop;
   if (scrollLocation > headerHeight - navHeight) {
-    navFix.style.display = 'block';
+    showNavigation('none');
+    header.classList.add('header--fixed');
   } else {
-    navFix.style.display = 'none';
+    showNavigation('block');
+    header.classList.remove('header--fixed');
   }
 });
+function showNavigation(display) {
+  naviWrapper.style.display = display;
+  innerMenu.style.display = display;
+}
 
-categoryMenuIcons.forEach((categoryMenuIcon) => {
-  categoryMenuIcon.addEventListener('click', () => {
-    category.classList.toggle('active');
-  });
+categoryMenuIcon.addEventListener('click', () => {
+  category.classList.toggle('active');
+  if (category.classList.contains('active')) {
+    categoryIconStyleToggle('black', 'white');
+    categoryMenuIcon.querySelector('i').classList.add('fa-x');
+  } else {
+    categoryIconStyleToggle('white', 'black');
+    categoryMenuIcon.querySelector('i').classList.remove('fa-x');
+  }
 });
+function categoryIconStyleToggle(backColor, fontColor) {
+  categoryMenuIcon.style.background = backColor;
+  categoryMenuIcon.style.color = fontColor;
+}
