78aa0d1df6638abfd662e49dd63a079c477e7e47
byeongjin <qudwls7468@gmail.com>
2023-02-26 16:12:27 +0900

    Design accounts myshop

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", views.signup, name="signup")
+    path("signup/", views.signup, name="signup"),
+    path("my_shop/", views.my_shop, name="my_shop")
 ]
