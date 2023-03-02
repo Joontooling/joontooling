159bd4c478c8d56cc0d6df44ceaea52f86dd4cea
byeongjin <qudwls7468@gmail.com>
2023-03-02 18:56:36 +0900

    Add community files notice and question

--- community/views.py
+++ community/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'community/index.html')
+
+def notice(request):
+    return render(request, 'community/notice.html')
+
+def question(request):
+    return render(request, 'community/question.html')
