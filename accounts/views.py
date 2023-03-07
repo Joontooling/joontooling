041466f80c173d8925326570621b4e49d1f4974d
jejoonlee <jejoonlee1996@gmail.com>
2023-03-07 14:40:45 +0900

    FEAT: 회원 수정, 비밀번호 수정, 탈퇴, 마이샵 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import UserForm
-from .models import User
+from .forms import UserForm, UserCustomChangeForm, CustomPasswordChangeForm
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -30,12 +31,10 @@ def login(request):
         form = AuthenticationForm(request, data = request.POST)
 
         if form.is_valid():
-            print("로그인 성공")
             auth_login(request, form.get_user())
             return redirect(request.GET.get('next') or 'products:index')
     
     else:
-        print("로그인 실")
         form = AuthenticationForm()
     
     context = {
@@ -44,8 +43,65 @@ def login(request):
 
     return render(request, 'accounts/login.html', context)
 
-def my_shop(request):
-    return render(request, 'accounts/my_shop.html')
+def logout(request):
+    auth_logout(request)
+    return redirect('products:index')
+
+@login_required
+def update(request, pk):
+
+    if request.user.pk == pk:
+    
+        if request.method == 'POST':
+            change_form = UserCustomChangeForm(request.POST, request.FILES, instance=request.user)
+            if change_form.is_valid():
+                change = change_form.save()
+                auth_login(request, user=change)
+                return redirect('accounts:my_shop', request.user.pk)
+        else:
+            change_form = UserCustomChangeForm(instance=request.user)
+    
+        context = {
+        'change_form': change_form,
+        }
+
+        return render(request, 'accounts/update.html', context)
+  
+    else:
+        return redirect('products:index')
+
+@login_required
+def password_update(request):
+    if request.method == 'POST':
+        password_form = CustomPasswordChangeForm(request.POST, request.user)
+        if password_form.is_valid():
+            user = password_form.save()
+            auth_login(request, user)
+            return redirect('accounts:update', request.user.pk)
+    else:
+        password_form = CustomPasswordChangeForm(request.user)
+
+    context = {
+        'password_form': password_form,
+    }
+    
+    return render(request, 'accounts/password.html', context)
+
+@login_required
+def delete(request, pk):
+    if request.user.pk == pk:
+        request.user.delete()
+    return redirect('products:index')
+
+@login_required
+def my_shop(request, pk):
+
+    if request.user.pk == pk:
+
+        context = {
+            'pk' : pk,
+        }
+    return render(request, 'accounts/my_shop.html', context)
 
 def my_likes(request):
     return render(request, 'accounts/my_likes.html')
